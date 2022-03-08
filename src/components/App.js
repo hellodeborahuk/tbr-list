7a7d1c25c9d45dcae0d6848854e8b33f10676330
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2022-03-08 11:42:41 +0000

    Setup basic auth for react app using supabase

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { AuthProvider } from '../contexts/Auth'
+import { Signup } from './Signup'
+import { Login } from './Login'
+import { Dashboard } from './Dashboard'
+import { PrivateRoute } from './PrivateRoute'
+
+export function App() {
+  return (
+    <div>
+      <h1>supabase-auth-react</h1>
+
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </div>
+  )
+}
---
+++ src/components/Dashboard.js
@@ -0,0 +1,23 @@
+import { useHistory } from 'react-router'
+import { useAuth } from '../contexts/Auth'
+
+export function Dashboard() {
+  const { user, signOut } = useAuth()
+
+  const history = useHistory()
+
+  async function handleSignOut() {
+    // Ends user session
+    await signOut()
+
+    // Redirects the user to Login page
+    history.push('/login')
+  }
+  
+    return (
+      <div>
+        <p>Welcome, {user?.id}!</p>
+        <button onClick={handleSignOut}>Sign out</button>
+      </div>
+    )
+  }
